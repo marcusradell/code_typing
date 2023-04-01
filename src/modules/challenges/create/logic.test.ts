39bd11f8cd550d400b2a088b9f744c96365beb8f
Marcus Rådell <marcus@radell.net>
2023-04-01 18:23:16 +0200

    test for level 3

--- src/modules/challenges/create/logic.test.ts
+++ src/modules/challenges/create/logic.test.ts
@@ -2,7 +2,7 @@ import { MONDAY, logic } from "./logic";
 
 test("Simple challenge is scored as level 1", () => {
   const args = {
-    id: "",
+    id: "qwfp-0099",
     name: "Simple challenge",
     content: "logger.info('Hi!')",
     todayWeekday: MONDAY + 1,
@@ -20,7 +20,7 @@ test("Simple challenge is scored as level 1", () => {
 
 test("Simple challenge on a Monday is scored as level 2", () => {
   const args = {
-    id: "",
+    id: "456-arst",
     name: "Simple Monday challenge",
     content: "logger.info('Hello!')",
     todayWeekday: MONDAY,
@@ -33,5 +33,25 @@ test("Simple challenge on a Monday is scored as level 2", () => {
     name: args.name,
     content: args.content,
     level: 2,
+  });
+});
+
+test("Hard challenge is scored as level 3", () => {
+  const args = {
+    id: "123-abc",
+    name: "Hard challenge",
+    content: `Lorem ipsum dolor sit amet;
+    consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt ut;`,
+    todayWeekday: MONDAY + 2,
+  };
+
+  const result = logic(args);
+
+  expect(result).toEqual({
+    id: args.id,
+    name: args.name,
+    content: args.content,
+    level: 3,
   });
 });
